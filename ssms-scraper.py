128536d9c124afa8b7157df5e362a6de4213bf65
Sinawo.Mngxuma <Sinawo.Mngxuma@canvasintelligence.com>
2023-05-17 14:13:47 +0200

    get all cars per page

--- ssms-scraper.py
+++ ssms-scraper.py
@@ -110,8 +110,8 @@ for page in range(num_iterations):
         
         # Find the link to the car listing
         for link in each_div.find_all('a', href=True):
-            if found_cars == 10:
-                 break
+            #if found_cars == 10:
+             #    break
             try:
                 found_link = (base_url + link['href'])
                 Car_ID = re.search(r'/(\d+)\?', found_link).group(1)
