fea6d834345d9c714e3eea50ecad5b04be5f41c3
Sinawo.Mngxuma <Sinawo.Mngxuma@canvasintelligence.com>
2023-05-29 12:15:42 +0200

    Added sql script with steps

--- AutoTrader_Schema.sql
+++ AutoTrader_Schema.sql
@@ -1,17 +1,10 @@
-select * from Car_Listing_Table
-select * from overnight_table
-select * from dealer_table
+--STEP 1: From the uncleaned table which is overnight_table lets take the new dataset and insert it to the Autotrader_sp
+INSERT INTO Autotrader_sp (Car_ID, Title, Price, [Car Type], [Registration Year], Mileage, Transmission, [Fuel Type], Dealership, Suburb, [Introduction date], [End date], [Engine position], [Engine detail], [Engine capacity (litre)], [Cylinder layout and quantity], [Fuel capacity], [Fuel consumption (average) **], [Fuel range (average)], [Power maximum (detail)], [Torque maximum], [Maximum/top speed], [CO2 emissions (average)], [Acceleration 0-100 km/h], [Last Updated], [Previous Owners], [Service History], [Colour], [Body Type])
+SELECT Car_ID, Title, Price, [Car Type], [Registration Year], Mileage, Transmission, [Fuel Type], Dealership, Suburb, [Introduction date], [End date], [Engine position], [Engine detail], [Engine capacity (litre)], [Cylinder layout and quantity], [Fuel capacity], [Fuel consumption (average) **], [Fuel range (average)], [Power maximum (detail)], [Torque maximum], [Maximum/top speed], [CO2 emissions (average)], [Acceleration 0-100 km/h], [Last Updated], [Previous Owners], [Service History], [Colour], [Body Type]
+FROM overnight_table
+WHERE Car_ID NOT IN (SELECT Car_ID FROM Autotrader_sp);
 
-Select * from Autotrader_Data
---- This is the Extracted dataset from Autotrader
-select * into Autotrader_Data
-from overnight_table
---- Clean the Title -- 
--- remove for sale
-update Autotrader_Data
-set Title = RTRIM(LTRIM(Replace(Title,'For Sale','')));
--- Remove the R in orice column
-update Autotrader
-set Price = Replace(Price,'R','');
+--STEP 2: Clean the data from the Autotrader_sp
+
 
 
