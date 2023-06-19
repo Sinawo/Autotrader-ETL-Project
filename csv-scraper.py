afbfdad0eab8bb1645c718bb154bc8a8419efd1d
Sinawo.Mngxuma <Sinawo.Mngxuma@canvasintelligence.com>
2023-06-19 11:28:43 +0200

    Updated script 4 new DB

--- csv-scraper.py
+++ csv-scraper.py
@@ -11,11 +11,13 @@ import datetime
 import os
 import math
 from datetime import datetime
+import csv 
+from pathlib import Path
 # URL of the Autotrader website
 base_url = "https://www.autotrader.co.za"
 
 MIN_YEAR = 1990
-
+csv_file_path = 'scraped_data.csv'
 # Define table and column names
 table_name = 'Autotrader_dataset'
 column_names = ['[Car_ID]', '[Title]', '[Price]', '[Car Type]', '[Registration Year]', '[Mileage]',
@@ -24,48 +26,14 @@ column_names = ['[Car_ID]', '[Title]', '[Price]', '[Car Type]', '[Registration Y
                 '[Cylinder layout and quantity]', '[Fuel capacity]', '[Fuel consumption (average) **]',
                 '[Fuel range (average)]', '[Power maximum (detail)]', '[Torque maximum]',
                 '[Maximum/top speed]', '[CO2 emissions (average)]', '[Acceleration 0-100 km/h]',
-                '[Last Updated]', '[Previous Owners]', '[Service History]', '[Colour]', '[Body Type]',  'latest_version', 'Time_stamp', 'First_Entry_Timestamp']
+                '[Last Updated]', '[Previous Owners]', '[Service History]', '[Colour]', '[Body Type]', 'First_Entry_Timestamp',  'latest_version', 'Time_stamp']
 
 column_n = ['Car_ID','Title', 'Price', 'Car Type', 'Body Type','Registration Year', 'Mileage', 'Transmission',
                 'Fuel Type', 'Dealership', 'Introduction date', 'End date','Suburb',
                 'Engine position', 'Engine detail', 'Engine capacity (litre)', 'Cylinder layout and quantity',
                 'Fuel capacity', 'Fuel consumption (average) **', 'Fuel range (average)',
                 'Power maximum (detail)', 'Torque maximum', 'Maximum/top speed', 'CO2 emissions (average)',
-                'Acceleration 0-100 km/h', 'Last Updated', 'Previous Owners', 'Service History', 'Colour', 'latest_version', 'Time_stamp','First_Entry_Timestamp']
-
-
-# define the connection details
-server = 'web-development.database.windows.net'
-database = 'graduates'
-username = 'Canvas'
-password = 'Dut950505'
-driver = '{ODBC Driver 17 for SQL Server}'
-
-# create a connection
-conn = pyodbc.connect(
-                      f'SERVER={server};'
-                      f'DATABASE={database};'
-                      f'UID={username};'
-                      f'PWD={password};'
-                      f'DRIVER={driver};')
-
-# create a cursor
-cursor = conn.cursor()
-
-# Combine column names with data types
-column_data_types = ['VARCHAR(MAX)'] * len(column_names)
-
-column_data_types[column_names.index('latest_version')] = 'INT DEFAULT 1'
-
-
-# Construct the CREATE TABLE query
-
-create_table_query = f"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = '{table_name}') \
-                       CREATE TABLE {table_name} ({', '.join(['{0} {1}'.format(name, data_type) for name, data_type in zip(column_names, column_data_types)])})"
-# Execute the CREATE TABLE query
-cursor.execute(create_table_query)
-# Commit the changes and close the connection
-conn.commit()
+                'Acceleration 0-100 km/h', 'Last Updated', 'Previous Owners', 'Service History', 'Colour','First_Entry_Timestamp' 'latest_version', 'Time_stamp']
 
 # List to store the links for each car
 car_links = []
@@ -117,12 +85,11 @@ def Add_Key_Values(list, dictionary):
 # Function to get the last page for a specific year
 def get_last_page(year):
     response = requests.get(f"https://www.autotrader.co.za/cars-for-sale?year={year}-to-{year}")
-        
+    print('Trying to get last page: ', response.status_code)  
     home_page = BeautifulSoup(response.content, 'lxml')
    
     total_listings_element = home_page.find('span', class_='e-results-total')
     total_listings = int(total_listings_element.text.replace(' ', ''))
-    
     # Determine the number of pages based on the number of listings per page (e.g., 24 listings per page)
     listings_per_page = 20
     last_page = math.ceil(total_listings / listings_per_page)
@@ -136,6 +103,7 @@ execution_time = time.time() + 3600
 # Starting page number and year
 start_page, start_year = get_last_scraped_page_and_year()
 year = start_year
+
 #last page for all total listings for a specific year 
 last_page = get_last_page(year)
 # Loop through each page of cars on the Autotrader website
@@ -150,8 +118,6 @@ for page in range(start_page, last_page + 1):
         page = 1
         year -= 1
         update_last_scraped_page_and_year(page, year)  # Reset to the first page
-        
-
 
     print(response.status_code)
     home_page = BeautifulSoup(response.content, 'lxml')
@@ -221,6 +187,7 @@ for page in range(start_page, last_page + 1):
             
             # Create a dictionary to store the details of the car
             car_data = {
+                'Car_ID': Car_ID,
                 'Title': title_element,
                 'Price': price,
                 'Car Type': car_type,
@@ -228,7 +195,8 @@ for page in range(start_page, last_page + 1):
                 'Mileage': mileage,
                 'Transmission': transmission,
                 'Fuel Type': fuel_type,
-                'Dealership': dealer_name
+                'Dealership': dealer_name,
+                'Suburb' : location
             }
             
             # Find the engine details section of the car listing page
@@ -238,40 +206,9 @@ for page in range(start_page, last_page + 1):
             car_data = Add_Key_Values(engine_details, car_data)
             car_data = Add_Key_Values(vehicle_details, car_data)
             
-            car_data['Car_ID'] = Car_ID
-            car_data['Suburb'] = location
-            car_data['Time_stamp'] = str(datetime.now())
             
             
-
-            # Check if the Car_ID exists
-            check_query = f"SELECT latest_version FROM {table_name} WHERE Car_ID = ?"
-            cursor.execute(check_query, (Car_ID))
-            result = cursor.fetchone()
-        
-            if result is not None:
-                  # If Car_ID exists, increment the Latest value
-                cursor.execute(f"UPDATE {table_name} SET Latest_version =  Latest_version + 1 \
-                               WHERE Car_ID = ? ", Car_ID)   
-            else:
-               
-                # If Car_ID doesn't exist, set the Latest value to 1
-                cursor.execute(f"UPDATE {table_name} SET Latest_version = 1\
-                               WHERE Car_ID = ? ", Car_ID)
-                
-            # Check if the Car_ID exists
-            check_query = f"SELECT First_Entry_Timestamp FROM {table_name} WHERE Car_ID = ?"
-            cursor.execute(check_query, (Car_ID))
-            result = cursor.fetchone()
-            if result is not None and result[0] is not None:
-                # Car_ID exists, do not modify 'First_Entry_Timestamp'
-                pass
-            else:
-                # Car_ID does not exist, add current timestamp to 'First_Entry_Timestamp'
-                car_data['First_Entry_Timestamp'] = car_data['Time_stamp']
-          
-
-                           
+            # car_data['Time_stamp'] = str(datetime.datetime.now())
 
             # Check if all values in car_data have corresponding column names [the one we want only]
             matching_keys = [key for key in car_data.keys() if key in column_n]
@@ -281,29 +218,21 @@ for page in range(start_page, last_page + 1):
 
             placeholders = ', '.join(['?'] * len(matching_keys))
             column_names_with_brackets = ', '.join('"' + key + '"'  for key in matching_keys)
-           
-            if result:
-                update_query = f"""
-                    UPDATE {table_name}
-                    SET {', '.join([f'"{key}" = ?' for key in matching_keys])}
-                    WHERE Car_ID = ?;
-                """
-                matching_values.append(Car_ID)  # Append Car_ID for the WHERE clause
-                cursor.execute(update_query, matching_values)
-            else:
-                # If Car_ID doesn't exist, set the Latest value to 1
-                insert_query = f"""
-                    INSERT INTO {table_name} (
-                        {column_names_with_brackets}
-                    )
-                    VALUES (
-                        {placeholders}
-                    );
-                """
-                cursor.execute(insert_query, matching_values)
+
+            # Check if the CSV file exists or not
+            csv_file_exists = Path(csv_file_path).is_file()
+            # Write the data to the CSV file
             
-            conn.commit()
+            with open(csv_file_path, 'a', newline='') as file:
+                writer = csv.writer(file)
+                # Write the header row if the CSV file doesn't exist
+                if not csv_file_exists:
+                    writer.writerow(column_names)
+                # Write the data row
+                writer.writerow(matching_values)
+           
+             
 
 
           
-conn.close()    
+
