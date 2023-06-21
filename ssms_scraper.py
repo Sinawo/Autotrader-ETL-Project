bf95f20e7f89c132fb66059ebf437596817465c0
MPMlangs <91530040+MPMlangs@users.noreply.github.com>
2023-06-21 11:11:48 +0200

    Update ssms_scraper.py

--- ssms_scraper.py
+++ ssms_scraper.py
@@ -85,17 +85,13 @@ def get_last_scraped_page_and_year():
             return last_page, last_year
     else:
         return 1, datetime.datetime.now().year  # Default values if the file doesn't exist
-# Function to update the last scraped page and year
-#def update_last_scraped_page_and_year(page, year):
-    #with open("last_page.txt", "w") as file:
-        #file.write(f"{page},{year}")
 
 def update_last_scraped_page_and_year(page, year):
     url = "https://api.github.com/repos/Sinawo/Webscraper_CI/contents/last_page.txt"
     branch = "Mukhethwa"
     username = "MPMlangs"
     password = "1996Ju16@"
-
+    
     # Get the current file content
     response = requests.get(url, params={"ref": branch}, auth=(username, password))
     response_json = response.json()
@@ -116,9 +112,9 @@ def update_last_scraped_page_and_year(page, year):
         print("File updated successfully.")
     else:
         print("An error occurred while updating the file.")
-      
+    
 # Function to convert a list to a dictionary
-def Convert(lst):
+def Convert(last):
     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
     return res_dct
 
@@ -128,32 +124,29 @@ def Add_Key_Values(list, dictionary):
     values = []           
     i = 2
     for detail in (list):
-                if i % 2 == 0:
-                    key = detail.text
-                    keys.append(detail.text)
-                else:
-                    value = detail.text
-                    values.append(detail.text)
-                    car_data[key] = value
-                i = i + 1
-                    # Add 'Time_stamp' and 'Latest' columns to the dictionary
-  
+        if i % 2 == 0:
+            key = detail.text
+            keys.append(detail.text)
+        else:
+            value = detail.text
+            values.append(detail.text)
+            car_data[key] = value
+            i = i + 1
+
+
     return car_data
+
 # Function to get the last page for a specific year
 def get_last_page(year):
     response = requests.get(f"https://www.autotrader.co.za/cars-for-sale?year={year}-to-{year}")
-        
     home_page = BeautifulSoup(response.content, 'lxml')
-   
     total_listings_element = home_page.find('span', class_='e-results-total')
     total_listings = int(total_listings_element.text.replace(' ', ''))
-    
+  
     # Determine the number of pages based on the number of listings per page (e.g., 24 listings per page)
     listings_per_page = 20
     last_page = math.ceil(total_listings / listings_per_page)
-    
     return last_page
-
 
 # Set the desired execution time to one hour (3600 seconds)
 execution_time = time.time() + 120
@@ -164,24 +157,20 @@ year = start_year
 #last page for all total listings for a specific year 
 last_page = get_last_page(year)
 # Loop through each page of cars on the Autotrader website
+
 #for page in range(num_iterations):
 for page in range(start_page, last_page + 1):
-
     response = requests.get(f"https://www.autotrader.co.za/cars-for-sale?pagenumber={page}&sortorder=PriceLow&year={year}-to-{year}&priceoption=RetailPrice")
-    
     # If you have finished scraping all pages for a specific year, 
-    # move to the next year and reset the last scraped page to 1
+    # move to the next year and reset the last scraped page to 
     if page == last_page:
         page = 1
         year -= 1
         update_last_scraped_page_and_year(page, year)  # Reset to the first page
         
-
-
     print(response.status_code)
     home_page = BeautifulSoup(response.content, 'lxml')
-    
-    
+
     # Find all the car listings on the page 
     cars_containers = home_page.find_all('div', attrs={'class': re.compile(r'b-result-tile .*')})
     # Loop through each car listing
@@ -190,41 +179,40 @@ for page in range(start_page, last_page + 1):
         # Find the link to the car listing   
         for link in each_div.find_all('a', href=True):
             if time.time() >= execution_time:
-              execution_time += 200
-              update_last_scraped_page_and_year(page, year) 
-              time.sleep(10)
+                execution_time += 120
+                update_last_scraped_page_and_year(page, year) 
+                time.sleep(10)
             try:
                 found_link = (base_url + link['href'])
                 # Extract the car ID using regular expression
                 Car_ID = re.search(r'/(\d+)\?', found_link).group(1)
-               
+
             except:
-                 continue
+                continue
 
             # Get the HTML content of the car listing page
             res = requests.get(found_link, timeout=5)
             soup = BeautifulSoup(res.content, 'lxml')
             
             ul_tag = soup.find('ul', class_='b-breadcrumbs')
-
             if ul_tag:
                 # Find the <a> tag within the <li> tag at position 3
                 a_tag = ul_tag.find_all('li')[2].find('a')
-
+                
                 if a_tag:
                     # Extract the text within the <a> tag
                     location = a_tag.get_text(strip=True)                   
                 else:
-                   pass
-            
+                    pass
+
             try: 
                 title_element = soup.find('h1', class_='e-listing-title').text.strip()
                 dealer_name = soup.find('a', attrs={'class': 'e-dealer-link'} ).text
             except:
-                 continue
+                continue
             price = soup.find('div', attrs={'class': 'e-price'}).text.strip()
             icons = soup.find('ul', class_='b-icons m-large m-icon').find_all('li')
-            
+
             car_type = icons[0].text.strip()
             if car_type == 'New Car': 
                 mileage = '0'
@@ -240,12 +228,11 @@ for page in range(start_page, last_page + 1):
 
             else:
                 mileage = icons[2].text.strip()
-
                 registration_year = icons[1].text.strip().split()[0]
                 transmission = icons[3].text.strip()
                 fuel_type = icons[4].text.strip()
-            
-            
+
+
             # Create a dictionary to store the details of the car
             car_data = {
                 'Title': title_element,
@@ -257,35 +244,34 @@ for page in range(start_page, last_page + 1):
                 'Fuel Type': fuel_type,
                 'Dealership': dealer_name
             }
-            
+
             # Find the engine details section of the car listing page
             engine_details = soup.find_all('span' , attrs={'class': 'col-6'})
             vehicle_details = soup.find_all('div' , attrs={'class': 'col-6'})
 
             car_data = Add_Key_Values(engine_details, car_data)
             car_data = Add_Key_Values(vehicle_details, car_data)
-            
+
             car_data['Car_ID'] = Car_ID
             car_data['Suburb'] = location
             car_data['Time_stamp'] = str(datetime.datetime.now())
-            
-            
+
+
 
             # Check if the Car_ID exists
             check_query = f"SELECT latest_version FROM {table_name} WHERE Car_ID = ?"
             cursor.execute(check_query, (Car_ID))
             result = cursor.fetchone()
-        
+
             if result is not None:
-                  # If Car_ID exists, increment the Latest value
+                # If Car_ID exists, increment the Latest value
                 cursor.execute(f"UPDATE {table_name} SET Latest_version =  Latest_version + 1 \
                                WHERE Car_ID = ? ", Car_ID)   
             else:
-               
                 # If Car_ID doesn't exist, set the Latest value to 1
                 cursor.execute(f"UPDATE {table_name} SET Latest_version = 1\
-                               WHERE Car_ID = ? ", Car_ID)
-                
+                             WHERE Car_ID = ? ", Car_ID)
+
             # Check if the Car_ID exists
             check_query = f"SELECT First_Entry_Timestamp FROM {table_name} WHERE Car_ID = ?"
             cursor.execute(check_query, (Car_ID))
@@ -296,19 +282,17 @@ for page in range(start_page, last_page + 1):
             else:
                 # Car_ID does not exist, add current timestamp to 'First_Entry_Timestamp'
                 car_data['First_Entry_Timestamp'] = car_data['Time_stamp']
-          
 
-                           
 
             # Check if all values in car_data have corresponding column names [the one we want only]
             matching_keys = [key for key in car_data.keys() if key in column_n]
-         
+
             # Get the corresponding values for the matching keys
             matching_values = [car_data[key] for key in matching_keys]
 
             placeholders = ', '.join(['?'] * len(matching_keys))
             column_names_with_brackets = ', '.join('"' + key + '"'  for key in matching_keys)
-           
+
             if result:
                 update_query = f"""
                     UPDATE {table_name}
@@ -328,11 +312,11 @@ for page in range(start_page, last_page + 1):
                     );
                 """
                 cursor.execute(insert_query, matching_values)
-            
+                
             conn.commit()
 
 
-          
+
 conn.close()  
 # Call the function to get the last scraped page and year
 last_page, last_year = get_last_scraped_page_and_year()
